3b0a77f303bc1031192483b7c38f8584de180251
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2015-07-12 16:21:13 -0300

    Create SGPreferences and add some methods to SGActivity.
    Signed-off-by: Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>

--- src/com/book/simplegameengine_v1/SGActivity.java
+++ src/com/book/simplegameengine_v1/SGActivity.java
@@ -1,17 +1,24 @@
 package com.book.simplegameengine_v1;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SGActivity extends Activity {
 	
 	public static final String TAG = "SimpleGameEngine";
+	private SGPreferences mPreferences;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		Log.v(TAG, "onCreate() chamado.");
+		mPreferences = new SGPreferences(this);
 	}
 	
 	@Override
@@ -49,5 +56,44 @@ public class SGActivity extends Activity {
 		super.onDestroy();
 		Log.v(TAG, "onDestroy() chamado.");
 	}
+	
+	public void enableKeepScreenOn(){
+		Window window = getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+	
+	public enum SGOrientation {
+		LANDSACPE,
+		POTRTRAIT
+	}
+	
+	public void setOrientation(SGOrientation orientation){
+		switch (orientation) {
+		case LANDSACPE:
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			break;
+		case POTRTRAIT:
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+		default:
+			break;
+		}
+	}
+	
+	@SuppressLint("NewApi")
+	public void enableFullScreen(){
+		Window window = getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		window.requestFeature(Window.FEATURE_NO_TITLE);
+		if(android.os.Build.VERSION.SDK_INT >= 19) {
+			window.getDecorView().setSystemUiVisibility( 
+					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | 
+					View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
+	}
+	
+	public SGPreferences getPreferences() {
+		return mPreferences;
+	}
+	
 }
