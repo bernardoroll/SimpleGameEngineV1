38b3ba6f739364b89cc0997d4459ee24c991fbbb
Bernardo Rocha Salgueiro <Bernardo@Bernardo-PC.Home>
2015-07-13 10:42:52 -0300

    Adição de movimento à bola e ao oponente, ainda constantes

---
+++ src/com/book/simplegameengine_v1/SGView.java
@@ -0,0 +1,48 @@
+package com.book.simplegameengine_v1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.view.View;
+
+public class SGView extends View {
+	
+	private Point mDimensions = new Point();
+	private boolean mHasStarted;
+	
+	public SGView(Context context) {
+		super(context);
+	}
+	
+	@Override
+	public void onDraw(Canvas canvas) {
+		canvas.drawColor(Color.LTGRAY);
+		step(canvas);
+		
+		invalidate();
+	}
+	
+	public void step(Canvas canvas) {
+		
+	}
+	
+	@Override
+	protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+		mDimensions.set(width, height);
+		if(!mHasStarted) {
+			setup();
+			mHasStarted = true;
+		}
+	}
+	
+	protected void setup() {
+		
+	}
+	
+	public Point getDimensions() {
+		return mDimensions;
+	}
+	
+
+}
